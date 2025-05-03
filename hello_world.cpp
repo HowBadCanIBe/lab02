0261c57ffc92b1c81b57288928091f6a6bde3479
HowBadCanIBe <svyatbogd@gmail.com>
2025-05-04 01:47:29 +0300

    Initial commit with Hello-World program

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,6 @@
- #include <iostream>
- #include <string>
- 
- using namespace std;
- 
- int main() {
-     string name;
-     cout << "Enter your name: ";
-     cin >> name;
-     cout << "Hello, World! From " << name << endl;
-     return 0;
- }
-
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
