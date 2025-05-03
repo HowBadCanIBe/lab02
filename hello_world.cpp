1a19132eec245f4bd7642c184c319223e7aa8503
HowBadCanIBe <svyatbogd@gmail.com>
2025-05-04 02:31:00 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,13 @@
+//Объявляю библиотеки
 #include <iostream>
 #include <string> 
+//Функция main
 int main() {
+//Ввод имени
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+//Печать hello world
     std::cout << "Hello, World! from " << name << std::endl;
     return 0;
 }
