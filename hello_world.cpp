8251c0cc7724cbf129c85526727bdaf7fd5f8961
HowBadCanIBe <svyatbogd@gmail.com>
2025-05-04 01:48:34 +0300

    Updated version of the Hello-World program

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+ #include <iostream>
+ #include <string>
+ using namespace std;
+ int main() {
+     string name;
+     cout << "Enter your name: ";
+     cin >> name;
+     cout << "Hello, World! from " << name << endl;
+     return 0;
+ }
